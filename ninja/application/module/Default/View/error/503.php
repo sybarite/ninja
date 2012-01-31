e258c76a7c4a90ee8c363c4b8fc7c61f43d46860
Dayson Pais <dayson@epicwhale.org>
2012-01-31 14:58:06 +0530

    - Improved design for 404 page not found and 503 exception pages.

--- ninja/application/module/Default/View/error/503.php
+++ ninja/application/module/Default/View/error/503.php
@@ -1,1 +1,61 @@
-Sorry, there seems to be a problem with the service. We will fix it shortly.
+<!doctype html>
+<!--[if lt IE 7 ]><html lang="en" class="no-js ie6"><![endif]-->
+<!--[if IE 7 ]><html lang="en" class="no-js ie7"><![endif]-->
+<!--[if IE 8 ]><html lang="en" class="no-js ie8"><![endif]-->
+<!--[if IE 9 ]><html lang="en" class="no-js ie9"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="UTF-8">
+    <meta name="robots" content="noindex"/>
+    <title>Service Unavailable</title>
+    <style type="text/css">
+        html {
+            height:100%;
+            -webkit-font-smoothing:antialiased;
+            border:0;
+            overflow-y:scroll;
+            margin:0;
+            padding:0;
+        }
+
+        body {
+            height:100%;
+            font-family:Arial,'Helvetica Neue',Helvetica,sans-serif;
+            text-align:center;
+            border:0;
+            background:#64829C no-repeat center;
+            margin:0;
+            padding:0;
+        }
+
+        h1 {
+            color:#fff;
+            font-size:30px;
+            font-weight:700;
+            text-shadow:0 1px 4px rgba(0,0,0,0.68);
+            letter-spacing:-1px;
+            margin:0;
+        }
+
+        .sorry {
+            color:#243748;
+            font-size:18px;
+            line-height:24px;
+            text-shadow:0 1px 1px rgba(255,255,255,0.25);
+        }
+        .sorry a{color: #243748;}
+    </style>
+</head>
+<body>
+<table height="100%" width="100%"><!-- // yes, we've used tables, but you better have your own version of this page ;-) -->
+    <tr>
+        <td align="center" valign="middle">
+            <h1>Service Unavailable</h1>
+            <div class="sorry" style="width:535px;">
+                <p>There was a problem processing your request.<br />Please try again later.</p>
+            </div>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
