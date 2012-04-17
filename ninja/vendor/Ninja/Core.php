a7703562dbfba6c1c17352d9eb5324835e568cd5
Dayson Pais <dayson@epicwhale.org>
2012-04-17 17:48:06 +0530

    Code clean up. Added necessary comments & fixed any violations of coding conventions.

--- ninja/vendor/Ninja/Core.php
+++ ninja/vendor/Ninja/Core.php
@@ -13,22 +13,19 @@ class Core
      */
 	public static $isCli;
 
-
     /**
      * Run on windows?
      * @var string
      */
 	public static $isWindows;
-	
-	
+
 	/**
-	* Whether Debug enabled or no
+	* Whether Debug mode enabled or not
 	* 
 	* @var boolean
 	*/
 	public static $debug = FALSE;
-	
-	
+
 	/**
 	* Access to all loaded configuration
 	* 
@@ -49,8 +46,7 @@ class Core
 	* @var string
 	*/
 	public static $currentUrl = '';
-	
-	
+
 	/**
 	* Instance of Ninja Router for handling routes
 	* @var \Ninja\Controller\Router
@@ -76,8 +72,7 @@ class Core
 	* @var \Ninja\Log
 	*/
 	public static $log;
-	
-	
+
 	/**
 	* Instance of internal \Ninja\ErrorReporter.php
 	* 
@@ -85,73 +80,81 @@ class Core
 	*/
 	public static $errorReporter;
 	
-	
-	private static $_init;
-	
-	
 	/**
-	* Initializes the Ninja-App Environment
-	* Common to both Web + Console Request
-	*/
-	private static function createApplication()
+     * Flag to check if initialized
+     * @var boolean
+     */
+	private static $_init;
+
+
+    /**
+     * Initializes the Ninja App Environment
+     * Common to both Web + Console Request
+     *
+     * @static
+     * @throws Exception
+     */
+    protected static function _createApplication()
 	{
-				
 		// Whether debug mode enabled or not?
-		if( defined('NINJA_DEBUG') )
+		if (defined('NINJA_DEBUG'))
+        {
 			self::$debug = NINJA_DEBUG;
+        }
 		
-		if( ! isset( self::$config['ninja']['name'] )  )
+		if (!isset(self::$config['ninja']['name']))
 		{
 			throw new \Ninja\Exception('The configuration ninja.name is not defined.');
 		}
 		
 		// Report ALL POSSIBLE ERRORS. Compatible with future versions of PHP.
+        /**
+         * Set the PHP error reporting level. If you set this in php.ini, you remove this.
+         * @see  http://php.net/error_reporting
+         *
+         * When developing your application, it is highly recommended to enable all notices
+         * and strict warnings. To report all PHP errors, set it to -1
+         *
+         * In a production environment, we should turn off all error reporting.
+         *
+         */
 		error_reporting(-1);
-		
-		
+
 		// Determine if we are running in a command line environment
     	self::$isCli = (PHP_SAPI === 'cli');
     	
     	// Determine if we are running in a Windows environment
     	self::$isWindows = (DIRECTORY_SEPARATOR === '\\');
-		
-		
+
 		// Check if the most evil magic quotes are enabled.
 		if ( (bool) get_magic_quotes_gpc() )
 			throw new \Ninja\Exception("Magic Quotes Enabled. Ninja refuses to fight!");
 		
 		// Check for register globals status
 		if ( ini_get('register_globals') )
-	    {
 	        throw new \Ninja\Exception("register_globals Enabled. Ninja refuses to fight!");
-	    }
 	    
-	    if( self::$debug  )
+	    if (self::$debug)
 	    {
+            /**
+             * In a production environment, we should turn on all error reporting (done above) and not display all errors on screen.
+             */
 			ini_set('display_errors', 'On');
 		}
 		else
 		{
+            /**
+             * In a production environment, we should turn off all error reporting and not display any errors on screen.
+             */
 			ini_set('display_errors', 'Off');
 			error_reporting(0);
 		}
 		
 		self::$log = new \Ninja\Log();
-		
-//		/**
-//         * Provide the debug function which is an easy to way to add messages to the debug log.
-//         *
-//         * @param $message
-//         * @param array $values
-//         */
-//		function debug($message, array $values = NULL)
-//		{
-//			\Ninja::$log->add(\Ninja\Log::DEBUG, $message, $values );
-//		}
 
-		/******Assumption: Upto this line, there is no scope for error**************/
-    
-    
+
+		/****** Assumption: Upto this line, there is no scope for error. So the error handler won't kick-in **************/
+
 		/**
 		* Define our custom error handler
 		*/
@@ -163,18 +166,18 @@ class Core
     	set_exception_handler( array('Ninja\Exception', 'Handler') );
 		
 		self::$errorReporter = new \Ninja\ErrorReporter();
-
 	}
 	
 	/**
      * Create a Ninja Web Application
+     *
      * @static
-     * @param  $config_file
+     * @param string $configFile
      * @return null
      */
-	public static function createWebApplication( $config_file )
+	public static function createWebApplication($configFile)
 	{
-		if( self::$_init )
+		if (self::$_init)
 		{
 			// Do not execute more than once
 			return;
@@ -188,26 +191,32 @@ class Core
         self::$autoLoader->registerNamespace('Ninja', NINJA_VENDOR_PATH . 'Ninja');
 
         // The config file may use the \Ninja\Config class, so autoloading should already be initialized by here
-		self::$config = require $config_file;
+		self::$config = require $configFile;
 
 		// Run common initialization		
-		self::createApplication();
+		self::_createApplication();
 
 		// Initialize router
 		self::$router = new \Ninja\Controller\Router();
 
+        /**
+         * Create the HTTP Request Object based on the request from the Web Server
+         */
         $request = \Ninja\Controller\Request\Http::createFromServerRequest();
 
-        if( isset( $_SERVER['SCRIPT_NAME'] ) )
+        if (isset( $_SERVER['SCRIPT_NAME']))
 		{
 			self::$baseUrl = substr($_SERVER['SCRIPT_NAME'], 0, strrpos($_SERVER['SCRIPT_NAME'] , '/') + 1 ); //find the full url to this application from server root
 		}
 		
 		self::$currentUrl = self::$baseUrl . $request->getRequestUri();
 
-        // load application/bootstrap.php here
+        // load application/bootstrap.php
 		self::doBootStrap(NINJA_APPLICATION_PATH . 'bootstrap.php');
 
+        /**
+         * Process the HTTP request object
+         */
         $routerProcessData = self::$router->processHttpRequest($request);
 
         // Initialize dispatcher
@@ -251,7 +260,6 @@ class Core
                     // if module does not have an error controller, then leave it to the default
                 }
 
-                
                 $errorRequest->setException($e)
                              ->setErrorRequest($request)
                              ->setModuleName('Default')
@@ -277,7 +285,7 @@ class Core
     private static function _dispatch(\Ninja\Controller\Dispatcher $dispatcher, \Ninja\Controller\Request\AbstractRequest $request, \Ninja\Controller\Response\Http $response)
     {
         ob_start();
-            $dispatcher->dispatch($request, $response);
+        $dispatcher->dispatch($request, $response);
         $dispatchOutput = ob_get_contents();
         ob_clean();
 
@@ -286,12 +294,13 @@ class Core
 
     /**
      * Create a Ninja Console Application
+     *
      * @static
-     * @throws \Ninja\Exception
-     * @param  $config_file
+     * @param string $configFile
      * @return null
+     * @throws Console\Exception
      */
-    public static function createConsoleApplication( $config_file )
+    public static function createConsoleApplication($configFile)
     {
         if( self::$_init )
 		{
@@ -309,14 +318,13 @@ class Core
         // Autoload the Ninja library from now on
         self::$autoLoader->registerNamespace('Ninja', NINJA_VENDOR_PATH . 'Ninja');
 
-
         // The config file may use the \Ninja\Config class, so autoloading should already be initialized by here
-		self::$config = require $config_file;
+		self::$config = require $configFile;
 
         // Run common initialization
-		self::createApplication();
+		self::_createApplication();
 
-        if( ! self::$isCli )
+        if (!self::$isCli)
             throw new \Ninja\Console\Exception("Cannot create console application when not in command line.");
 
         // load application/bootstrap.php here
@@ -325,20 +333,18 @@ class Core
         // Register the 'Console' namespace at application/console
         self::$autoLoader->registerNamespace('Console', NINJA_APPLICATION_PATH . 'cli/Console');
 
-
+        // Retreive all parameters passed
         $args = $_SERVER['argv'];
         self::$commandRunner = new \Ninja\Console\Command\Runner($args[0]);
 
         array_shift($args);
         self::$commandRunner->run($args);
-
-        
     }
 
     /**
      * Loads a file in an insolated space
      * @static
-     * @param  $file file name
+     * @param string $file file name
      * @return void
      */
 	private static function doBootStrap($file)
@@ -346,8 +352,7 @@ class Core
         // Load the bootstrap file, this allows routes, etc to be defined before the request is parsed and executed
 		include $file;
 	}
-	
-	
+
 	/**
      * Access configuration array using a dot separator path.
      * For example \Ninja::$config['database']['default']['password'] can be accessed as:
@@ -362,5 +367,4 @@ class Core
 	{
 		return \Ninja\Config::path(self::$config, $path, $default , '.');
 	}
-	
 }
